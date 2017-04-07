0b01ec975fbe7253cd196179c005836235893f4e
ilutschii <wicki96@gmail.com>
2017-04-07 23:11:45 +0200

    index.xhtml now shows only 100 temperatures in the graph instead of all

--- src/main/java/at/wickenhauser/PoolTempServer/ManagedBeans/DashBoardBean.java
+++ src/main/java/at/wickenhauser/PoolTempServer/ManagedBeans/DashBoardBean.java
@@ -9,6 +9,7 @@ import at.wickenhauser.PoolTempServer.entity.Temperature;
 import at.wickenhauser.PoolTempServer.facades.TemperatureFacade;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.LinkedList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -101,6 +102,7 @@ public class DashBoardBean implements Serializable{
         LineChartSeries series1 = new LineChartSeries();
         series1.setLabel("Temperatur");
         int i=0;
+        tempList=minimizeList(tempList,100);
         for (Temperature temperature : tempList) {
             series1.set(sdf.format(temperature.getTime()),temperature.getTemperature());
         }
@@ -129,6 +131,34 @@ public class DashBoardBean implements Serializable{
         return model;
     }
     
-    
+ private List<Temperature> minimizeList(List<Temperature> fullList, int maxSize) {
+        Temperature temp1;
+        Temperature temp2;
+        List<Temperature> minimizedList = new LinkedList<>();
+        
+        while (fullList.size() >= maxSize) {
+            minimizedList = new LinkedList<>();
+            for (int i = 0; i < fullList.size(); i = i + 2) {
+                if (i + 1 < fullList.size()) {
+                    temp1 = fullList.get(i);
+                    temp2 = fullList.get(i + 1);
+                    temp1.setTemperature((temp1.getTemperature()+ temp2.getTemperature()) / 2);
+                    minimizedList.add(temp1);
+                }
+            }
+            fullList = minimizedList;
+/*
+            temp1=result.get(0);
+            temp2=result.get(1);
+            result.remove(0);
+            result.remove(0);
+            temp1.setTemp((temp1.getTemp()+temp2.getTemp())/2);
+            minimizedList.add(temp1);
+            */
+        }
+        if(minimizedList.size()==0)
+            return fullList;
+        return minimizedList;
+    }
     
 }
